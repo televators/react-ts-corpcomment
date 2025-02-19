655c0c398ee3c956ee349207a1aca944f96a6770
Justin Higgs <justinphiggs@gmail.com>
2025-02-18 23:49:54 -0500

    Refining FeedbackList after being away for a while. Added react-spinners for list loading state.

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
 import FeedbackListItem from "./FeedbackListItem";
 import { FeedbackItem } from "../types/types";
+import { MoonLoader } from "react-spinners";
+
+interface FeedbackResponse {
+  feedbacks: FeedbackItem[];
+}
 
 const FeedbackList = () => {
+  const [isLoading, setIsLoading] = useState( true );
   const [feedbackItems, setFeedbackItems] = useState<FeedbackItem[] | null>( [] );
 
   useEffect( () => {
-    async function getFeedbacks(): Promise<FeedbackItem[]> {
+    async function getFeedbacks(): Promise<FeedbackResponse> {
       try {
         const response = await fetch( 'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks' );
 
@@ -33,14 +39,31 @@ const FeedbackList = () => {
         console.log( data );
         setFeedbackItems( data.feedbacks );
       } )
-      .catch( error => console.error( error ) );
+      .catch( error => console.error( error ) )
+      .finally( () => setIsLoading( false ) );
   }, [] );
 
   return (
-    <ol className="feedback-list">
-      { feedbackItems.map( ( feedbackItem ) => (
-        <FeedbackListItem key={ feedbackItem.id } feedbackItem={ feedbackItem } />
-      ) ) }
+    <ol className={`feedback-list ${ ( ! feedbackItems || feedbackItems.length === 0 ) ? 'feedback-list--empty' : '' }`}>
+      { isLoading ? (
+        <MoonLoader
+          color="#695390"
+          size={100}
+          aria-label="Loading icon"
+          cssOverride={{
+            marginTop: '3rem',
+          }}
+        />
+      ) : (
+        <>
+          { feedbackItems && feedbackItems.map( ( feedbackItem ) => (
+            <FeedbackListItem key={ feedbackItem.id } feedbackItem={ feedbackItem } />
+          ) ) }
+          { ! feedbackItems && (
+            <h4 className="feedback-list__no-data-message">No feedback yet. Submit one above!</h4>
+          ) }
+        </>
+      ) }
     </ol>
   );
 }
